1e6601943480778c23aa4f807bac4c341575c83d
Faraji Ombonya <farajiombonya@gmail.com>
2024-04-21 21:15:50 +0300

    feat: basic form

---
+++ src/layouts/MainCanvas.jsx
@@ -0,0 +1,5 @@
+function MainCanvas({ children }) {
+  return <div className="container mx-auto px-4 max-w-screen-lg">{children}</div>;
+}
+
+export default MainCanvas;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import DiagnosisForm from "../components/DiagnosisForm";
+import MainCanvas from "../layouts/MainCanvas";
+
+function LandingPage() {
+  return (
+    <>
+      <MainCanvas>
+        <DiagnosisForm />
+      </MainCanvas>
+    </>
+  );
+}
+
+export default LandingPage;
